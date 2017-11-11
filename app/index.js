04e02c6508031dce69404f102dd4916f38c82f88
Toby Hoshman <t.hoshman@gmail.com>
2017-11-10 17:18:25 -0800

    designed frontend

--- app/index.js
+++ app/index.js
@@ -12,7 +12,30 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>React is bae</div>
+      <div>
+        <h2>Silver Screen Suggestor</h2>
+        <br></br>
+        <div>Who is an actor you like?</div>
+        <form>
+          <input type='text' /><input type="button" />
+        </form>
+        <br></br>
+        <br></br>
+        <h3>Your Favorite Actors</h3>
+        <ul>
+          {this.state.actors.map((actor) => (
+            <li>{actor}</li>
+            ))}
+        </ul>
+        <br></br>
+        <br></br>
+        <h3>Movies you might like</h3>
+        <ul>
+          {this.state.movies.map((movie) => (
+            <li><a href={movie.url}>{movie.title}</a></li>
+            ))}
+        </ul>
+      </div>
       )
   }
 }
